57715aadb23f99d43fc131e9dc0c19713db61838
Abdulrahimch <44474691+Abdulrahimch@users.noreply.github.com>
2021-06-30 18:30:32 +0300

    Update oauth.js

--- server/controllers/oauth.js
+++ server/controllers/oauth.js
@@ -6,17 +6,13 @@ const generator = require('generate-password');
 // @desc Login if user exists else Register user
 // @access Private
 exports.loginRegisterUser = asyncHandler(async (req, res, next) => {
-    console.log('username emails is ', req.user.email)
-    // check if the user already exists.
     let user = await User.findOne({ email: req.user.email })
 
     if (!user) {
         user = new User({ username: req.user.displayName, email: req.user.email })
         user.password = generator.generate({ length: 10, numbers: true })
-        console.log('user is: ', user)
 
         try {
-            console.log('lets go to save')
             await user.save()
         } catch(err) {
             res.send(err)
@@ -32,7 +28,6 @@ exports.loginRegisterUser = asyncHandler(async (req, res, next) => {
           }
         });
     }
-    // Login
     else {
         res.status(200).json({
           success: {
