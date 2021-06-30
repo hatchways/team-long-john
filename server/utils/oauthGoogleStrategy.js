3123f0759b25d2a0f82f8bbf572490088ac4d465
Abdulrahimch <44474691+Abdulrahimch@users.noreply.github.com>
2021-06-30 18:32:01 +0300

    Update oauthGoogleStrategy.js

--- server/utils/oauthGoogleStrategy.js
+++ server/utils/oauthGoogleStrategy.js
@@ -8,7 +8,6 @@ passport.use(new GoogleStrategy({
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
-   console.log(accessToken)
     return done(null, profile)
   }
 ));
