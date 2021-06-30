f0b2913a4fb2187e37ff568a78f043f3aa3cf680
abdulraim <abdulrahimcisco92@gmail.com>
2021-06-30 12:15:02 +0300

    Created controllers and routes to get and update users

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
-const { searchUsers } = require("../controllers/user");
+const { searchUsers, getUser, updateUser } = require("../controllers/user");
 
 router.route("/").get(protect, searchUsers);
+
+router.route("/users/:username").get(getUser);
+
+router.route("/users/:id").patch(updateUser);
 
 module.exports = router;
