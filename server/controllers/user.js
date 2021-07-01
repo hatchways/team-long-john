08ee3c2ae9258d19fba99d176d59bbcec9199989
abdulraim <abdulrahimcisco92@gmail.com>
2021-07-01 10:02:19 +0300

    using res.json instead of res.send

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -29,27 +29,41 @@ exports.getUser = asyncHandler(async (req, res, next) => {
     const username = req.params.username
     const user = await User.findOne({ username });
 
-    if (!user) res.status(404).send({ error: "User not found" });
-    else res.status(200).send(user);
+    if (!user) {
+        res.status(404);
+        throw new Error("User not found" );
+    }
+
+    res.status(200).json({
+        success: {
+          user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            register_date: user.register_date
+       }
+    }
+  });
 });
 
 // @route PATCH /users/:id
 // @desc Update user
 // @access Private
 exports.updateUser = asyncHandler(async (req, res, next) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['username', 'email', 'password'];
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-    if (!isValidOperation) return res.status(404).send({ error: 'Invalid update' });
+    const id = req.params.id;
+    updates = req.body;
+    options = { new: true };
 
-    const user = await User.findById({ _id: req.params.id });
-    if (!user) return res.status(404).send({ error: "User not found" });
+    const user = await User.findByIdAndUpdate(id, updates, options);
 
-    try{
-        updates.forEach((update) => user[update] = req.body[update]);
-        await user.save();
-        res.send(user);
-    } catch (e) {
-        res.status(400).send(e);
+    res.status(200).json({
+        success: {
+          user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            register_date: user.register_date
+       }
     }
+  });
 });
