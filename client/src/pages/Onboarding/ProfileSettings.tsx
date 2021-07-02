425bea4ea2d98e3878e8a27a72067704bba228cb
Andy Garcia <acgarzeah@gmail.com>
2021-07-02 00:45:26 -0700

    ffinished styling for profile_settings page

--- client/src/pages/Onboarding/ProfileSettings.tsx
+++ client/src/pages/Onboarding/ProfileSettings.tsx
@@ -1,33 +1,79 @@
 import React from 'react';
-import { Grid, Box, InputAdornment, InputLabel, OutlinedInput } from '@material-ui/core/';
+import moment from 'moment-timezone';
+
+import {
+  Grid,
+  Box,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Select,
+  MenuItem,
+  FormControl,
+} from '@material-ui/core/';
 
 import OnboardingHeader from './OnboardingHeader/OnboardingHeader';
 import logo from '../../Images/logo.png';
 import useStyles from './useStyles';
+import { string } from 'yup';
+
+interface TimeZone {
+  [key: string]: { timeZone: string; abbr: string };
+}
 
 const ProfileSettings = (): JSX.Element => {
   const classes = useStyles();
 
+  const filteredTimeZones: TimeZone = {};
+
+  // Filtering out list of unnecessary time zones (lots of duplicates)
+  moment.tz.names().map((timeZone): void => {
+    const abbr = moment.tz(timeZone).zoneName();
+
+    if (abbr[0] === '+' || abbr[0] === '-') return;
+
+    filteredTimeZones[abbr] = { timeZone, abbr };
+    return;
+  });
+
+  // Render our list of time zones to choose from
+  const renderTimeZones = Object.keys(filteredTimeZones)
+    .sort()
+    .map((key) => {
+      const { timeZone, abbr } = filteredTimeZones[key];
+      const time = moment.tz(timeZone).format('(h:ma)');
+
+      return (
+        <MenuItem key={abbr} value={timeZone}>
+          <h4 style={{ display: 'inline', marginRight: '5px' }}>{abbr} Time</h4>
+          <span style={{ color: '#b0b9cd' }}>{time}</span>
+        </MenuItem>
+      );
+    });
+
   return (
     <Grid className={classes.page} container direction="column" justify="center" alignItems="center">
       <img src={logo} />
-      <Box className={classes.component}>
+      <Box className={classes.box}>
         <OnboardingHeader headerText="Welcome to CalendApp!" />
-        <Box className={classes.form}>
-          <Box>
-            <h4 className={classes.subheader}>Create your CalendApp URL:</h4>
-            <h4 className={classes.subheader}>Select your time zone:</h4>
+        <Box mt={5}>
+          <Box mb={3} mx={10} className={classes.inputBox}>
+            <h4>Create your CalendApp URL:</h4>
+            <FormControl>
+              <OutlinedInput
+                startAdornment={
+                  <InputAdornment position="start">
+                    <InputLabel>calendapp.com/</InputLabel>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </Box>
-          <Box className={classes.box}>
-            <OutlinedInput
-              id="input-with-icon-adornment"
-              startAdornment={
-                <InputAdornment position="start">
-                  <InputLabel htmlFor="component-outlined">calendapp.com/</InputLabel>
-                </InputAdornment>
-              }
-            />
-            <OutlinedInput id="input-with-icon-adornment" />
+          <Box mb={2} mx={10} className={classes.inputBox}>
+            <h4>Select your time zone:</h4>
+            <FormControl style={{ minWidth: '35%' }} variant="outlined">
+              <Select value="">{renderTimeZones}</Select>
+            </FormControl>
           </Box>
         </Box>
       </Box>
--- client/src/pages/Onboarding/useStyles.ts
+++ client/src/pages/Onboarding/useStyles.ts
@@ -2,24 +2,20 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
   page: {
+    width: '100vw',
     height: '100vh',
+    padding: '0 40px',
   },
-  component: {
+  box: {
     borderRadius: 5,
     height: '60vh',
     width: '70vh',
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
   },
-  form: {
+  inputBox: {
     display: 'flex',
-    justifyContent: 'center',
-  },
-  box: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  subheader: {
-    marginRight: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 }));
 
