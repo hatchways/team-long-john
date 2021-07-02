17cc22586ea7e0947020c21107048549378f0508
Andy Garcia <acgarzeah@gmail.com>
2021-07-02 02:08:56 -0700

    finished front end for profile_settings

--- client/src/pages/Onboarding/ProfileSettings.tsx
+++ client/src/pages/Onboarding/ProfileSettings.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import moment from 'moment-timezone';
 
 import {
-  Grid,
   Box,
   InputAdornment,
   InputLabel,
@@ -10,12 +9,12 @@ import {
   Select,
   MenuItem,
   FormControl,
+  Button,
 } from '@material-ui/core/';
 
+import CalendAppLogo from '../../components/CalendAppLogo/CalendAppLogo';
 import OnboardingHeader from './OnboardingHeader/OnboardingHeader';
-import logo from '../../Images/logo.png';
 import useStyles from './useStyles';
-import { string } from 'yup';
 
 interface TimeZone {
   [key: string]: { timeZone: string; abbr: string };
@@ -23,8 +22,15 @@ interface TimeZone {
 
 const ProfileSettings = (): JSX.Element => {
   const classes = useStyles();
-
   const filteredTimeZones: TimeZone = {};
+
+  // Sends a GET request to check for a unique URL
+  // Add a randomly generated unique url in the default value
+  // If not taken, then send a PUT request for username url and time zone
+  // const handleClickContinue = async () => {};
+
+  // Set up later redirects to dashboard
+  // const handleClickSetUpLater = () => {};
 
   // Filtering out list of unnecessary time zones (lots of duplicates)
   moment.tz.names().map((timeZone): void => {
@@ -41,7 +47,7 @@ const ProfileSettings = (): JSX.Element => {
     .sort()
     .map((key) => {
       const { timeZone, abbr } = filteredTimeZones[key];
-      const time = moment.tz(timeZone).format('(h:ma)');
+      const time = moment.tz(timeZone).format('(h:mma)');
 
       return (
         <MenuItem key={abbr} value={timeZone}>
@@ -52,32 +58,40 @@ const ProfileSettings = (): JSX.Element => {
     });
 
   return (
-    <Grid className={classes.page} container direction="column" justify="center" alignItems="center">
-      <img src={logo} />
+    <Box mt={5} className={classes.page}>
+      <CalendAppLogo />
       <Box className={classes.box}>
-        <OnboardingHeader headerText="Welcome to CalendApp!" />
-        <Box mt={5}>
-          <Box mb={3} mx={10} className={classes.inputBox}>
-            <h4>Create your CalendApp URL:</h4>
-            <FormControl>
-              <OutlinedInput
-                startAdornment={
-                  <InputAdornment position="start">
-                    <InputLabel>calendapp.com/</InputLabel>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
+        <div>
+          <OnboardingHeader headerText="Welcome to CalendApp!" />
+          <Box mt={5} className={classes.form}>
+            <Box mb={3} mx={10} className={classes.inputBox}>
+              <h4 style={{ marginRight: '15px' }}>Create your CalendApp URL:</h4>
+              <FormControl>
+                <OutlinedInput
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <InputLabel>calendapp.com/</InputLabel>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Box>
+            <Box mb={2} mx={10} className={classes.inputBox}>
+              <h4>Select your time zone:</h4>
+              <FormControl style={{ minWidth: '35%' }} variant="outlined">
+                <Select defaultValue="">{renderTimeZones}</Select>
+              </FormControl>
+            </Box>
           </Box>
-          <Box mb={2} mx={10} className={classes.inputBox}>
-            <h4>Select your time zone:</h4>
-            <FormControl style={{ minWidth: '35%' }} variant="outlined">
-              <Select value="">{renderTimeZones}</Select>
-            </FormControl>
-          </Box>
+        </div>
+        <Box mb={3} className={classes.buttonBox}>
+          <Button variant="contained" className={classes.finish}>
+            Continue
+          </Button>
+          <Button className={classes.setUpLater}>Set up later</Button>
         </Box>
       </Box>
-    </Grid>
+    </Box>
   );
 };
 
--- client/src/pages/Onboarding/useStyles.ts
+++ client/src/pages/Onboarding/useStyles.ts
@@ -1,21 +1,49 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { findByLabelText } from '@testing-library/react';
 
 const useStyles = makeStyles(() => ({
   page: {
     width: '100vw',
     height: '100vh',
-    padding: '0 40px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   box: {
     borderRadius: 5,
     height: '60vh',
     width: '70vh',
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
   },
   inputBox: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  buttonBox: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  finish: {
+    color: 'white',
+    backgroundColor: '#F76900',
+    height: '55px',
+    width: '200px',
+    marginBottom: '10px',
+  },
+  setUpLater: {
+    color: 'lightgray',
+    height: '55px',
+    width: '200px',
+    marginBottom: '10px',
   },
 }));
 
