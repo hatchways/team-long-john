fc93d8f2fbec32d8360b01f71a601d52f9158dae
Andy Garcia <acgarzeah@gmail.com>
2021-07-06 14:42:32 -0700

    Fixed formatting

--- server/app.js
+++ server/app.js
@@ -25,24 +25,24 @@ const app = express();
 const server = http.createServer(app);
 
 cloudinary.config({
-	cloud_name: "calend-app",
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
-	secure: true
+  cloud_name: "calend-app",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
 
 const io = socketio(server, {
-	cors: {
-		origin: "*"
-	}
+  cors: {
+    origin: "*"
+  }
 });
 
 io.on("connection", (socket) => {
-	console.log("connected");
+  console.log("connected");
 });
 
 if (process.env.NODE_ENV === "development") {
-	app.use(logger("dev"));
+  app.use(logger("dev"));
 }
 app.use(json());
 app.use(urlencoded({ extended: false }));
@@ -53,8 +53,8 @@ app.use(passport.session());
 app.use(express.static(join(__dirname, "public")));
 
 app.use((req, res, next) => {
-	req.io = io;
-	next();
+  req.io = io;
+  next();
 });
 
 app.use("/auth", authRouter);
@@ -64,15 +64,15 @@ app.use("/appointment", appointmentRouter);
 app.use("/meeting", meetingRouter);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
-	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname), "client", "build", "index.html")
-	);
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname), "client", "build", "index.html")
+  );
 } else {
-	app.get("/", (req, res) => {
-		res.send("API is running");
-	});
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
 }
 
 app.use(notFound);
@@ -80,9 +80,9 @@ app.use(errorHandler);
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-	console.log(`Error: ${err.message}`.red);
-	// Close server & exit process
-	server.close(() => process.exit(1));
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process 
+  server.close(() => process.exit(1));
 });
 
 module.exports = { app, server };
