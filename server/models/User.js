6184f6ad3ef7837532cbcdf838f9e637d51e3b21
Andy Garcia <acgarzeah@gmail.com>
2021-07-06 17:21:18 -0700

    Fixed formatting

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		unique: true
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	register_date: {
-		type: Date,
-		default: Date.now
-	}
+  username: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  register_date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = User = mongoose.model('user', userSchema);
+module.exports = User = mongoose.model("user", userSchema);
