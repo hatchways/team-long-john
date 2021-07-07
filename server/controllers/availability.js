1325ed9e46c3dab568523c5e1f0c5a80e3118782
Andy Garcia <acgarzeah@gmail.com>
2021-07-07 16:48:51 -0700

    Updated name of user query

--- server/controllers/availability.js
+++ server/controllers/availability.js
@@ -9,19 +9,19 @@ exports.getAvailability = asyncHandler(async (req, res, next) => {
   const _id = "60e516b5f6562f2e3ea2df12";
 
   // ISO is a date format that eliminates ambiguity
-  const iso = new Date("2021-11-07T05:35:58.397Z");
+  const iso = new Date("2021-7-07T05:35:58.397Z");
   const day = iso.toLocaleString("default", { weekday: "long" });
 
-  // Check if the user is available on this day
   const user = await User.findById(_id);
   const { username, availableDays } = user;
 
-  // Check if the user is available on this day
   if (!(availableDays.indexOf(day) in availableDays)) {
     res.status(406);
     throw new Error(`${username} is not available on ${day}s`);
   }
 
+  // Google Calendar API, return list of available slots
+  // Get the current time in the user's timezone and return remaining times for this day
   // Get all the users appointments
   // Fetch all appointments and filter them out for this day
 });
