29dee61ab4fb51e1d75bdcb5c53fda5f4b2ad133
Luke Kim <lkdh97@gmail.com>
2021-07-07 22:00:15 -0400

    All of the functionalities for the time change and the scheduling options are complete. Skeletons for the appropriate changes from different timezone to the user's own timezone is set. Still need to implement available days.

--- client/src/pages/Scheduler/Scheduler.tsx
+++ client/src/pages/Scheduler/Scheduler.tsx
@@ -5,15 +5,13 @@ import { Box } from '@material-ui/core';
 import useStyles from './useStyles';
 import { Typography } from '@material-ui/core';
 import { Button } from '@material-ui/core';
-import { useAuth } from '../../context/useAuthContext';
-import { useSocket } from '../../context/useSocketContext';
 import { useHistory } from 'react-router-dom';
-import { useEffect } from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { mockLoggedInUser } from '../../mocks/mockUser';
 import Calendar from 'react-calendar';
-import './Calendar.css';
 import moment from 'moment-timezone';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 interface disableDateProp {
   activeStartDate: Date;
@@ -21,50 +19,175 @@ interface disableDateProp {
   view: string;
 }
 
+interface TimeZone {
+  [key: string]: { tZone: string; abbr: string };
+}
+
 export default function Scheduler(): JSX.Element {
   const history = useHistory();
   const classes = useStyles();
-  const timeZone = 'America/Toronto';
-  const today = new Date();
 
+  const username = 'TEMP USER';
+  const duration = 60;
+  const availableDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  const userTimeZone = 'America/Toronto';
+  const startTime = '11:20';
+  const endTime = '20:00';
+
+  const today = new Date();
+  const [timeZone, setTimeZone] = useState(userTimeZone);
   const [dateSelected, setDateSelected] = useState(today);
   const [dateSelISO, setDateSelISO] = useState(today.toISOString());
 
+  // This prop is used to set custom menu for the Select pop-up.
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: 400,
+        width: 250,
+      },
+    },
+  };
+
+  // Create a map of time zones.
+  const timeZones: TimeZone = {};
+  moment.tz.names().map((tZone): void => {
+    const abbr = moment.tz(tZone).zoneName();
+
+    if (abbr[0] === '+' || abbr[0] === '-') return;
+
+    timeZones[tZone] = { tZone, abbr };
+  });
+
+  // Render our list of time zones to choose from.
+  const renderTimeZones = Object.keys(timeZones)
+    .sort()
+    .map((key) => {
+      const { tZone, abbr } = timeZones[key];
+      const time = moment.tz(tZone).format('(h:mma)');
+
+      return (
+        <MenuItem key={tZone} value={tZone}>
+          <h4 style={{ display: 'inline', marginRight: '5px' }}> {`${tZone} (${abbr})`} </h4>
+          <span style={{ color: '#b0b9cd' }}>{time}</span>
+        </MenuItem>
+      );
+    });
+
+  const changeTimeZone = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setTimeZone(event.target.value as string);
+  };
+
   const updateDate = (event: Date) => {
+    // Since clicking on the calendar gives the date object in the local timezone,
+    // only extract the year, month, and date info from the event.
+    // Then create a moment using the timezone and get ISOString.
+    const dateInfo = moment(event).format('YYYY-MM-DD');
+    const momentTZ = moment.tz(dateInfo, timeZone);
     setDateSelected(event);
-    setDateSelISO(event.toISOString());
   };
 
   const disableDates = (dateProp: disableDateProp) => {
-    const date = dateProp.date;
-    let dateNum = date.getFullYear() * 10000 + date.getMonth() * 100;
-    let todayNum = today.getFullYear() * 10000 + today.getMonth() * 100;
-    if (dateProp.view !== 'year') {
-      dateNum += date.getDate();
-      todayNum += today.getDate();
-    }
-    return dateNum < todayNum;
+    // Turn the prop.date amd today into moment with appropriate timezone.
+    const dateInfo = moment(dateProp.date).format('YYYY-MM-DD');
+    const dateMoment = moment.tz(dateInfo, timeZone);
+    const todayMoment = moment.tz(today, timeZone);
+    return dateMoment.isBefore(todayMoment, 'day');
   };
 
-  const timeZoneInfo = () => {
-    return `${timeZone} (${moment.tz(today, timeZone).format('Z z')})`;
+  const calenDateToUserTZ = (timeValue: string) => {
+    const dateInfo = moment(dateSelected).format('YYYY-MM-DD');
+    const momentTZ = moment.tz(`${dateInfo} ${timeValue}`, timeZone);
+    const userMoment = moment.tz(momentTZ, userTimeZone);
+    return userMoment;
+  };
+
+  const checkDisableTime = (timeValue: string) => {
+    // Put further disabling based on user's google calendar info here.
+    const userMoment = calenDateToUserTZ(timeValue);
+    return userMoment.isBefore(moment(today));
+  };
+
+  const timeButtonBuilder = (curMoment: moment.Moment, endMoment: moment.Moment) => {
+    const output = [];
+    while (curMoment.isBefore(endMoment)) {
+      const time = curMoment.format('HH:mm');
+      output.push(
+        <Button
+          key={time}
+          value={time}
+          className={classes.timeSched}
+          onClick={getDateInfo}
+          disabled={checkDisableTime(time)}
+        >
+          {time}
+        </Button>,
+      );
+      curMoment = curMoment.add(duration, 'm');
+    }
+    return output;
+  };
+
+  const getDateInfo = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const timeValue = event.currentTarget.value;
+    const userMoment = calenDateToUserTZ(timeValue);
+
+    // Set the DateSelISO here so we can easily send request along with time.
+    setDateSelISO(userMoment.toISOString());
+    alert(`Time selected on user's base time zone is ${userMoment}`);
+  };
+
+  const populateTimes = () => {
+    // Using moment with user's own timezone as the basis, get a moment of the same time in different timezone.
+    // Then observe how HH:mm changed to create buttons based on the selected timezone.
+    const availStartBase = moment.tz(`${moment(today).format('YYYY-MM-DD')} ${startTime}`, userTimeZone);
+    const availEndBase = moment.tz(`${moment(today).format('YYYY-MM-DD')} ${endTime}`, userTimeZone);
+
+    const availStartMoment = moment.tz(availStartBase, timeZone);
+    const availEndMoment = moment.tz(availEndBase, timeZone);
+
+    const availStartTZ = availStartMoment.format('HH:mm');
+    const availEndTZ = availEndMoment.format('HH:mm');
+
+    const compareTimeA = new Date(`1/1/2020 ${availStartTZ}`);
+    const compareTimeB = new Date(`1/1/2020 ${availEndTZ}`);
+
+    if (compareTimeA < compareTimeB) {
+      return timeButtonBuilder(moment(compareTimeA), moment(compareTimeB));
+    } else if (compareTimeB < compareTimeA) {
+      const firstHalf = timeButtonBuilder(
+        moment(new Date(`1/1/2020 00:${startTime.split(':')[1]}`)),
+        moment(compareTimeB),
+      );
+      const secondHalf = timeButtonBuilder(moment(compareTimeA), moment(new Date('1/1/2020 23:59')));
+      return firstHalf.concat(secondHalf);
+    }
   };
 
   return (
     <Grid className={`${classes.root}`}>
       <CssBaseline />
       <Box className={classes.wrapper}>
-        <Box className={classes.meetingInfo} />
+        <Box className={classes.meetingInfo}>
+          <Typography className={classes.meetingInfoUser}> {username} </Typography>
+          <Typography className={classes.meetingInfoHeader}> {duration} minute meeting </Typography>
+          <ScheduleIcon className={classes.meetingInfoTime} />
+          <Typography className={classes.meetingInfoTime}> {duration} min </Typography>
+        </Box>
         <Box className={classes.calendarContainer}>
           <Typography className={classes.calendarHeader}> Select a Date &amp; Time </Typography>
-          <Calendar
-            value={dateSelected}
-            minDetail={'year'}
-            showNeighboringMonth={false}
-            onChange={updateDate}
-            tileDisabled={disableDates}
-          />
-          <Typography className={classes.calendarTimeZone}> {timeZoneInfo()} </Typography>
+          <Calendar minDetail={'year'} showNeighboringMonth={false} onChange={updateDate} tileDisabled={disableDates} />
+          <FormControl style={{ minWidth: '35%' }}>
+            <Select defaultValue={userTimeZone} MenuProps={MenuProps} onChange={changeTimeZone}>
+              {renderTimeZones}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box className={classes.timeContainer}>
+          <Typography className={classes.timeHeader}>
+            {moment.tz(dateSelected, userTimeZone).format('dddd, MMMM DD')}
+          </Typography>
+          <Box className={classes.timeSchedContainer}> {populateTimes()} </Box>
         </Box>
       </Box>
     </Grid>
--- client/src/pages/Scheduler/useStyles.ts
+++ client/src/pages/Scheduler/useStyles.ts
@@ -1,8 +1,156 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-// const drawerWidth = 240;
+// const drawerWidth = 240,
 
 const useStyles = makeStyles({
+  '@global': {
+    '::-webkit-scrollbar': {
+      width: '10px',
+    },
+    '::-webkit-scrollbar-track': {
+      background: '#f1f1f1',
+      borderRadius: '15px',
+    },
+    '::-webkit-scrollbar-thumb': {
+      background: 'lightgray',
+      borderRadius: '15px',
+    },
+    '::-webkit-scrollbar-thumb:hover': {
+      background: 'gray',
+      borderRadius: '15px',
+    },
+    '.react-calendar': {
+      width: '450px',
+      maxWidth: '100%',
+      background: 'white',
+      fontFamily: 'Arial, Helvetica, sans-serif',
+      lineHeight: '1.125em',
+      marginTop: '30px',
+      marginBottom: '30px',
+    },
+    '.react-calendar--doubleView': {
+      width: '700px',
+    },
+    '.react-calendar--doubleView .react-calendar__viewContainer': {
+      display: 'flex',
+      margin: '-0.5em',
+    },
+    '.react-calendar--doubleView .react-calendar__viewContainer > *': {
+      width: '50%',
+      margin: '0.5em',
+    },
+    '.react-calendar, .react-calendar *, .react-calendar *:before, .react-calendar *:after': {
+      boxSizing: 'border-box',
+    },
+    '.react-calendar button': {
+      margin: '0',
+      border: '0',
+      outline: 'none',
+    },
+    '.react-calendar button:enabled:hover': {
+      cursor: 'pointer',
+    },
+    '.react-calendar__navigation': {
+      height: '44px',
+      marginTop: '1em',
+      marginBottom: '1em',
+    },
+    '.react-calendar__navigation button': {
+      minWidth: '44px',
+      background: 'none',
+      fontSize: '1.5em',
+      fontWeight: 'bold',
+      color: 'gray',
+    },
+    '.react-calendar__navigation button:enabled:hover, .react-calendar__navigation button:enabled:focus': {
+      color: 'white',
+      backgroundColor: '#f76b00',
+    },
+    '.react-calendar__navigation button[disabled]': {
+      backgroundColor: '#f0f0f0',
+    },
+    '.react-calendar__navigation button.react-calendar__navigation__next-button': {
+      color: '#f76b00',
+    },
+    '.react-calendar__navigation button.react-calendar__navigation__next-button:enabled:hover': {
+      color: 'white',
+      backgroundColor: '#f76b00',
+    },
+    '.react-calendar__navigation button.react-calendar__navigation__next2-button': {
+      color: '#f76b00',
+    },
+    '.react-calendar__navigation button.react-calendar__navigation__next2-button:enabled:hover': {
+      color: 'white',
+      backgroundColor: '#f76b00',
+    },
+    '.react-calendar__month-view__weekdays': {
+      textAlign: 'center',
+      textTransform: 'uppercase',
+      fontWeight: 'bold',
+    },
+    '.react-calendar__month-view__weekdays__weekday': {
+      padding: '0.5em',
+    },
+    '.react-calendar__month-view__weekNumbers': {
+      fontWeight: 'bold',
+    },
+    '.react-calendar__month-view__weekNumbers .react-calendar__tile': {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '0.75em',
+      padding: 'calc(0.75em / 0.75) calc(0.5em / 0.75)',
+    },
+    '.react-calendar__month-view__days': {
+      height: '300px',
+    },
+    '.react-calendar__month-view__days__day--weekend': {
+      color: '#d10000',
+    },
+    '.react-calendar__month-view__days__day--neighboringMonth': {
+      color: 'white',
+    },
+    '.react-calendar__year-view .react-calendar__tile, .react-calendar__decade-view .react-calendar__tile, .react-calendar__century-view .react-calendar__tile':
+      {
+        padding: '2em 0.5em',
+      },
+    '.react-calendar__tile': {
+      maxWidth: '100%',
+      textAlign: 'center',
+      padding: '0.75em 0.75em',
+      borderRadius: '30%',
+      background: 'none',
+      fontWeight: 'bold',
+    },
+    '.react-calendar__tile:disabled': {
+      color: 'lightgray',
+    },
+    '.react-calendar__tile:enabled:hover, .react-calendar__tile:enabled:focus': {
+      backgroundColor: '#e6e6e6',
+    },
+    '.react-calendar__tile--hasActive': {
+      color: 'white',
+      background: '#f76b00',
+    },
+    '.react-calendar__tile--hasActive:enabled:hover, .react-calendar__tile--hasActive:enabled:focus': {
+      color: 'white',
+      background: '#f76b00',
+    },
+    '.react-calendar__tile--active': {
+      background: '#f76900',
+      color: 'white',
+    },
+    '.react-calendar__tile--active:enabled:hover, .react-calendar__tile--active:enabled:focus': {
+      background: '#f76b00',
+    },
+    '.react-calendar--selectRange .react-calendar__tile--hover': {
+      backgroundColor: '#e6e6e6',
+    },
+    '.react-calendar abbr': {
+      fontSize: '1.2em',
+      textDecoration: 'none',
+    },
+  },
   root: {
     position: 'absolute',
     minHeight: '100vh',
@@ -23,15 +171,32 @@ const useStyles = makeStyles({
     boxShadow: '0 6px 20px 0 rgba(0, 0, 0, 0.19)',
   },
   meetingInfo: {
+    overflow: 'auto',
+    position: 'relative',
     flex: 'left',
-    height: '600px',
+    height: '640px',
     width: '360px',
+    padding: '50px',
     borderRight: 'solid 2px #eff1f7',
   },
+  meetingInfoUser: {
+    fontSize: '1.5em',
+    color: 'lightgray',
+  },
+  meetingInfoHeader: {
+    fontWeight: 'bold',
+    fontSize: '2em',
+    marginBottom: '20px',
+  },
+  meetingInfoTime: {
+    float: 'left',
+    paddingLeft: '3px',
+    fontWeight: 'bold',
+  },
   calendarContainer: {
-    flex: 'left',
-    height: '520px',
     position: 'relative',
+    flex: 'left',
+    height: '560px',
     margin: '40px',
     paddingTop: 'auto',
     paddingBottom: 'auto',
@@ -44,6 +209,32 @@ const useStyles = makeStyles({
   },
   calendarTimeZone: {
     fontWeight: 'bold',
+  },
+  timeContainer: {
+    position: 'relative',
+    flex: 'left',
+    height: '600px',
+    width: '30%',
+    margin: '20px',
+    paddingTop: '90px',
+    paddingBottom: 'auto',
+    border: 'none',
+  },
+  timeHeader: {
+    fontWeight: 'bold',
+    fontSize: '1.5em',
+    color: 'gray',
+  },
+  timeSchedContainer: {
+    height: '400px',
+    marginTop: '30px',
+    overflow: 'auto',
+  },
+  timeSched: {
+    width: '250px',
+    height: '50px',
+    marginBottom: '10px',
+    border: 'solid 2px lightgray',
   },
 });
 
