695f3165e8aaeb83bfbd8f76016a6b2d26176511
Andy Garcia <acgarzeah@gmail.com>
2021-07-08 16:13:38 -0700

    Updated user model to keep track of google refresh token

--- server/models/User.js
+++ server/models/User.js
@@ -3,42 +3,45 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    unique: true,
+    unique: true
   },
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   register_date: {
     type: Date,
-    default: Date.now,
+    default: Date.now
   },
   timezone: {
-    type: String,
+    type: String
   },
   availableHours: {
     type: {
       times: {
         start: String,
-        end: String,
-      },
+        end: String
+      }
     },
     default: {
       start: "08:00",
-      end: "17:00",
-    },
+      end: "17:00"
+    }
   },
   availableDays: {
     type: [String],
-    default: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    default: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
   },
   appointments: [
     {
       type: mongoose.Types.ObjectId,
-      ref: "appointment",
-    },
+      ref: "appointment"
+    }
   ],
+  googleRefreshToken: {
+    type: String
+  }
 });
 
 module.exports = User = mongoose.model("user", userSchema);
