b48870b7bf152cc04241dbb699e7b2bc00247705
Andy Garcia <acgarzeah@gmail.com>
2021-07-09 22:17:56 -0700

    Moved redirect logic from AuthSetUp.tsx to useAuthContext.ts

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -24,8 +24,13 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
 
   const updateLoginContext = useCallback(
     (data: AuthApiDataSuccess) => {
-      setLoggedInUser(data.user);
-      history.push('/dashboard');
+      if (!data.user.username) {
+        setLoggedInUser(data.user);
+        history.push('profile_settings');
+      } else {
+        setLoggedInUser(data.user);
+        history.push('/dashboard');
+      }
     },
     [history],
   );
@@ -46,7 +51,6 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
       await loginWithCookies().then((data: AuthApiData) => {
         if (data.success) {
           updateLoginContext(data.success);
-          history.push('/dashboard');
         } else {
           // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
           setLoggedInUser(null);
--- client/src/pages/helper/AuthSetUp.tsx
+++ client/src/pages/helper/AuthSetUp.tsx
@@ -1,10 +1,8 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import { useAuth } from '../../context/useAuthContext';
-import { useHistory } from 'react-router-dom';
 
 export default function AuthSetUp(): JSX.Element {
-  const history = useHistory();
   const { updateLoginContext } = useAuth();
 
   const url = '/users/me';
@@ -20,17 +18,8 @@ export default function AuthSetUp(): JSX.Element {
         alert('Req.user is undefined');
       }
     })
-    .then((data) => {
-      updateLoginContext(data.success);
-      if (data.success.user.username) {
-        history.push('/dashboard');
-      } else {
-        history.push('/profile_settings');
-      }
-    })
-    .catch((error) => {
-      alert(error);
-    });
+    .then((data) => updateLoginContext(data.success))
+    .catch((error) => alert(error));
 
   return <Grid />;
 }
