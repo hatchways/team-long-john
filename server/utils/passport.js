bad379a7ec6305aa0b4c34cbf83d9175943060f1
Andy Garcia <acgarzeah@gmail.com>
2021-07-10 20:13:20 -0700

    Updated how we save and update a user

--- server/utils/passport.js
+++ server/utils/passport.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
@@ -19,16 +20,19 @@ passport.use(
       if (existingUser) {
         await User.findOneAndUpdate(
           { email: profile.emails[0].value },
-          { $set: { googleRefreshToken: refreshToken } }
+          { $set: { google: { refreshToken: refreshToken } } }
         );
 
         return done(null, existingUser);
       }
 
       await new User({
-        username: profile.displayName,
+        name: profile.displayName,
         email: profile.emails[0].value,
-        googleRefreshToken: refreshToken
+        google: {
+          id: profile.id,
+          refreshToken: refreshToken
+        }
       }).save();
 
       return done(null, profile);
@@ -40,8 +44,12 @@ passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
+passport.deserializeUser(async function (id, done) {
+  if (mongoose.isValidObjectId(id)) {
+    const oldUser = await User.findById(id);
+    done(null, oldUser);
+  } else {
+    const newUser = await User.findOne({ "google.id": id });
+    done(null, newUser);
+  }
 });
