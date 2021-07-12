fda943492d13ea4e5e98f03b0f9c6a7a7ce95c03
Luke Kim <lkdh97@gmail.com>
2021-07-12 18:49:20 -0400

    Moving initiate login and initiate signup to the APICalls folder for better management.

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,14 +1,12 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import { FormikHelpers } from 'formik';
 import useStyles from './useStyles';
-import register from '../../helpers/APICalls/register';
 import { useAuth } from '../../context/useAuthContext';
 import { useSnackBar } from '../../context/useSnackbarContext';
 import React from 'react';
 import GetStarted from '../helper/GetStarted/GetStarted';
 import AuthenticateMenu from '../helper/AuthenticateMenu/AuthenticateMenu';
-import { generateKeyPair } from 'crypto';
+import initiateSignUp from '../../helpers/APICalls/signup';
 
 export default function Register(): JSX.Element {
   const classes = useStyles();
@@ -22,33 +20,6 @@ export default function Register(): JSX.Element {
     setUserEmail(event.currentTarget.value.toLowerCase());
   };
 
-  const initiateSignUp = () => {
-    // We need to check if there is already an account with the associated userEmail.
-    const url = '/users';
-    const request = new Request(url, {
-      method: 'POST',
-      body: JSON.stringify({
-        email: userEmail,
-      }),
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    });
-    fetch(request)
-      .then((res) => {
-        if (res && res.status === 200) {
-          alert('An user with the given email exists. Please try logging in with this email.');
-        } else if (res && res.status === 404) {
-          setValidated(true);
-        }
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  };
-
   const diffEmail = () => {
     // This event handler is invoked when the user wants to choose a different email.
     setValidated(false);
@@ -60,7 +31,12 @@ export default function Register(): JSX.Element {
       {validated ? (
         <AuthenticateMenu signup={true} email={userEmail} diffEmail={diffEmail} />
       ) : (
-        <GetStarted signup={true} redirTarget="/login" textChange={textChange} initiater={initiateSignUp} />
+        <GetStarted
+          signup={true}
+          redirTarget="/login"
+          textChange={textChange}
+          initiater={() => initiateSignUp(userEmail, setValidated)}
+        />
       )}
     </Grid>
   );
