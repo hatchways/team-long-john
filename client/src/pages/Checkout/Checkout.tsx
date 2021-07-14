b4920447c97ab4cc44d27986e51579f972b9c23f
Andy Garcia <acgarzeah@gmail.com>
2021-07-13 23:09:26 -0700

    Finished styling for the checkout page

--- client/src/pages/Checkout/Checkout.tsx
+++ client/src/pages/Checkout/Checkout.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react';
-import { Box, FormControl, OutlinedInput, Button } from '@material-ui/core';
+import { Box, FormControl, OutlinedInput, Button, MenuItem, Select } from '@material-ui/core';
 
+import Navigation from '../Navigation/Navigation';
 import { useSnackBar } from '../../context/useSnackbarContext';
 import useStyles from './useStyles';
 
 interface CardInfo {
   cardNumber: string;
-  expirationDate: string;
+  expirationMonth: string;
+  expirationYear: string;
   cvc: string;
 }
 
 const Checkout = (): JSX.Element => {
   const classes = useStyles();
-  const [cardInfo, setCardInfo] = useState<CardInfo>({ cardNumber: '', expirationDate: '', cvc: '' });
+  const [cardInfo, setCardInfo] = useState<CardInfo>({
+    cardNumber: '',
+    expirationMonth: '',
+    expirationYear: '',
+    cvc: '',
+  });
+
+  const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+  const years = ['21', '22', '23', '24', '25', '26', '27', '28', '29', '30'];
 
   const { updateSnackBarMessage } = useSnackBar();
 
@@ -22,39 +32,79 @@ const Checkout = (): JSX.Element => {
   };
 
   const handleClick = (): any => {
-    const { cardNumber, expirationDate, cvc } = cardInfo;
+    const { cardNumber, expirationMonth, expirationYear, cvc } = cardInfo;
 
-    if (cardNumber.trim() === '' || expirationDate.trim() === '' || cvc.trim() === '') {
+    if (
+      cardNumber.trim() === '' ||
+      expirationMonth.trim() === '' ||
+      expirationYear.trim() === '' ||
+      cvc.trim() === ''
+    ) {
       return updateSnackBarMessage('Please enter valid input');
     }
   };
 
+  const renderMonths = months.map((month) => (
+    <MenuItem key={month} value={month}>
+      {month}
+    </MenuItem>
+  ));
+
+  const renderYears = years.map((year) => (
+    <MenuItem key={year} value={year}>
+      {year}
+    </MenuItem>
+  ));
+
   return (
     <Box className={classes.page}>
-      <h1>Checkout</h1>
-      <Box>
-        <Box className={classes.cardNumberBox}>
+      <Navigation />
+      <Box my={5} px={30} py={10} className={classes.checkoutBox}>
+        <Box className={classes.headerBox}>
+          <h1>Checkout</h1>
+        </Box>
+        <Box mb={2.5} className={classes.cardNumberBox}>
           <h3>Card number</h3>
           <FormControl>
-            <OutlinedInput name="cardNumber" value={cardInfo.cardNumber} onChange={(e) => handleChange(e)} />
+            <OutlinedInput
+              placeholder="7364 8257 2746 6153"
+              name="cardNumber"
+              value={cardInfo.cardNumber}
+              onChange={(e) => handleChange(e)}
+            />
           </FormControl>
         </Box>
         <Box className={classes.cardInfoBox}>
-          <Box className={classes.cardExpirationBox}>
-            <h3>Expiry date</h3>
-            <FormControl>
-              <OutlinedInput name="expirationDate" value={cardInfo.expirationDate} onChange={(e) => handleChange(e)} />
-            </FormControl>
+          <Box>
+            <h3>Expiration date</h3>
+            <Box style={{ border: '1px solid #bcbcbc', borderRadius: '5px', display: 'flex' }}>
+              <Box>
+                <FormControl
+                  variant="outlined"
+                  className={`${classes.selectButton} ${classes.root}`}
+                  style={{ borderRight: '1px solid #bcbcbc' }}
+                >
+                  <Select value={cardInfo.expirationMonth}>{renderMonths}</Select>
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl variant="outlined" className={`${classes.selectButton} ${classes.root}`}>
+                  <Select value={cardInfo.expirationYear}>{renderYears}</Select>
+                </FormControl>
+              </Box>
+            </Box>
           </Box>
-          <Box className={classes.cardCVCBox}>
+          <Box>
             <h3>CVC</h3>
-            <FormControl>
-              <OutlinedInput name="cvc" value={cardInfo.cvc} onChange={(e) => handleChange(e)} />
+            <FormControl style={{ minWidth: '100px' }}>
+              <OutlinedInput placeholder="•••" name="cvc" value={cardInfo.cvc} onChange={(e) => handleChange(e)} />
             </FormControl>
           </Box>
         </Box>
         <Box mt={5} className={classes.buttonBox}>
-          <Button className={classes.upgradeButton}>Pay $5</Button>
+          <Button onClick={handleClick} className={classes.upgradeButton}>
+            Pay $5
+          </Button>
         </Box>
       </Box>
     </Box>
--- client/src/pages/Checkout/useStyles.ts
+++ client/src/pages/Checkout/useStyles.ts
@@ -6,23 +6,33 @@ const useStyles = makeStyles(() => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
+  checkoutBox: {
+    overflow: 'hidden',
+    height: '60vh',
+    width: '80vw',
+    boxShadow: '0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+  },
+  headerBox: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
   cardNumberBox: {
+    minWidth: '200px',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
   },
   cardInfoBox: {
     display: 'flex',
-  },
-  cardExpirationBox: {
-    width: '30vw',
-  },
-  cardCVCBox: {
-    width: '20vw',
+    justifyContent: 'space-between',
   },
   buttonBox: {
     display: 'flex',
     justifyContent: 'center',
+  },
+  selectButton: {
+    minWidth: '100px',
+    border: 'none',
   },
   upgradeButton: {
     color: 'white',
@@ -30,6 +40,11 @@ const useStyles = makeStyles(() => ({
     height: '55px',
     width: '150px',
     margin: '15px 0',
+  },
+  root: {
+    '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+      border: 'none',
+    },
   },
 }));
 
