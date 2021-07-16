a9d9b06224cd9072c793e09122adf1e769ba857b
Andy Garcia <acgarzeah@gmail.com>
2021-07-15 23:45:14 -0700

    Converted checkout page to use formik

--- client/src/pages/Checkout/Checkout.tsx
+++ client/src/pages/Checkout/Checkout.tsx
@@ -1,48 +1,27 @@
-import { useState } from 'react';
-import { Box, FormControl, OutlinedInput, Button, MenuItem, Grid, Select, InputLabel } from '@material-ui/core';
+import { useFormik } from 'formik';
+import { Box, FormControl, OutlinedInput, Button, Grid } from '@material-ui/core';
 
 import Navigation from '../Navigation/Navigation';
 import { useSnackBar } from '../../context/useSnackbarContext';
 import useStyles from './useStyles';
 
-interface CardInfo {
-  cardNumber: string;
-  expirationMonth: string;
-  expirationYear: string;
-  cvc: string;
-}
-
 const Checkout = (): JSX.Element => {
   const classes = useStyles();
-  const [cardInfo, setCardInfo] = useState<CardInfo>({
-    cardNumber: '',
-    expirationMonth: '',
-    expirationYear: '',
-    cvc: '',
-  });
-
-  const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-  const years = ['21', '22', '23', '24', '25', '26', '27', '28', '29', '30'];
-
   const { updateSnackBarMessage } = useSnackBar();
-
-  const handleChange = (e: { target: HTMLInputElement | HTMLTextAreaElement }) => {
-    const { name, value } = e.target;
-    setCardInfo({ ...cardInfo, [name]: value });
-  };
-
-  const handleClick = (): void => {
-    const { cardNumber, expirationMonth, expirationYear, cvc } = cardInfo;
-
-    if (
-      cardNumber.trim() === '' ||
-      expirationMonth.trim() === '' ||
-      expirationYear.trim() === '' ||
-      cvc.trim() === ''
-    ) {
-      return updateSnackBarMessage('Please enter a valid card');
-    }
-  };
+  const formik = useFormik({
+    initialValues: {
+      number: '',
+      month: '',
+      year: '',
+      cvc: '',
+    },
+    onSubmit: async ({ number, month, year, cvc }, { resetForm }) => {
+      if (number.trim() === '' || month.trim() === '' || year.trim() === '' || cvc.trim() === '') {
+        return updateSnackBarMessage('Please enter valid card credentials');
+      }
+      // resetForm();
+    },
+  });
 
   return (
     <Box className={classes.page}>
@@ -51,76 +30,72 @@ const Checkout = (): JSX.Element => {
         <Grid container justify="center">
           <h1>Checkout</h1>
         </Grid>
-        <Grid container justify="center">
-          <Box>
-            <h3>Card number</h3>
-            <FormControl>
-              <OutlinedInput
-                placeholder="7364 8257 2746 6153"
-                name="cardNumber"
-                value={cardInfo.cardNumber}
-                onChange={(e) => handleChange(e)}
-                style={{ minWidth: '400px' }}
-              />
-            </FormControl>
-          </Box>
-        </Grid>
-        <Box style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Box>
-            <h3>Expiration Date</h3>
-            <Box style={{ display: 'flex', justifyContent: 'flex-start' }}>
-              <Box>
-                <FormControl>
-                  <OutlinedInput
-                    placeholder="08"
-                    name="expirationMonth"
-                    value={cardInfo.expirationMonth}
-                    onChange={(e) => handleChange(e)}
-                    onInput={(e: any) => {
-                      e.target.value
-                        ? (e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 2))
-                        : null;
-                    }}
-                    style={{ maxWidth: '100px' }}
-                  />
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl>
-                  <OutlinedInput
-                    placeholder="24"
-                    name="expirationYear"
-                    value={cardInfo.expirationYear}
-                    onChange={(e) => handleChange(e)}
-                    onInput={(e: any) => {
-                      e.target.value
-                        ? (e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 2))
-                        : null;
-                    }}
-                    style={{ maxWidth: '100px' }}
-                  />
-                </FormControl>
+        <form onSubmit={formik.handleSubmit}>
+          <Grid container justify="center">
+            <Box>
+              <h3>Card number</h3>
+              <FormControl>
+                <OutlinedInput
+                  placeholder="7364 8257 2746 6153"
+                  name="number"
+                  value={formik.values.number}
+                  onChange={formik.handleChange}
+                  inputProps={{ maxLength: 16 }}
+                  style={{ minWidth: '400px' }}
+                />
+              </FormControl>
+            </Box>
+          </Grid>
+          <Box style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Box>
+              <h3>Expiration Date</h3>
+              <Box style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                <Box>
+                  <FormControl>
+                    <OutlinedInput
+                      placeholder="08"
+                      name="month"
+                      value={formik.values.month}
+                      onChange={formik.handleChange}
+                      inputProps={{ maxLength: 2 }}
+                      style={{ maxWidth: '100px' }}
+                    />
+                  </FormControl>
+                </Box>
+                <Box>
+                  <FormControl>
+                    <OutlinedInput
+                      placeholder="24"
+                      name="year"
+                      value={formik.values.year}
+                      onChange={formik.handleChange}
+                      inputProps={{ maxLength: 2 }}
+                      style={{ maxWidth: '100px' }}
+                    />
+                  </FormControl>
+                </Box>
               </Box>
             </Box>
+            <Box ml={5}>
+              <h3>CVC</h3>
+              <FormControl>
+                <OutlinedInput
+                  placeholder="•••"
+                  name="cvc"
+                  value={formik.values.cvc}
+                  onChange={formik.handleChange}
+                  inputProps={{ maxLength: 3 }}
+                  style={{ maxWidth: '100px' }}
+                />
+              </FormControl>
+            </Box>
           </Box>
-          <Box ml={5}>
-            <h3>CVC</h3>
-            <FormControl>
-              <OutlinedInput
-                placeholder="•••"
-                name="cvc"
-                value={cardInfo.cvc}
-                onChange={(e) => handleChange(e)}
-                style={{ maxWidth: '100px' }}
-              />
-            </FormControl>
+          <Box mt={2.5} className={classes.buttonBox}>
+            <Button type="submit" className={classes.upgradeButton}>
+              Pay $5
+            </Button>
           </Box>
-        </Box>
-        <Box mt={2.5} className={classes.buttonBox}>
-          <Button onClick={handleClick} className={classes.upgradeButton}>
-            Pay $5
-          </Button>
-        </Box>
+        </form>
       </Box>
     </Box>
   );
