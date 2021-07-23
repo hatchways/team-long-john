4bdb50ee7537eff0da23daf8dfca869c42170499
Andy Garcia <acgarzeah@gmail.com>
2021-07-22 20:11:47 -0700

    Updated passport.js

--- utils/passport.js
+++ utils/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${isDev()}/auth/google/callback`,
+      callbackURL: `${process.env.PROD_URL}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({
