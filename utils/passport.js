81712ff5ed61acdc71d38096e7ea7cfec9536fbf
Andy Garcia <acgarzeah@gmail.com>
2021-07-22 19:40:13 -0700

    Updated callbackURL

--- utils/passport.js
+++ utils/passport.js
@@ -4,13 +4,14 @@ const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
 const User = require("../models/User");
 const { userCreationEmail } = require("./emailSender");
+const isDev = require("./isDev");
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: `${isDev()}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({
